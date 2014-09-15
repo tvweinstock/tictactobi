1e415009b23baa3f4894b771a3e77211c4642a7a
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-14 22:35:52 -0400

    tutorial steps removed, different logic to be implemented

--- scripts.js
+++ scripts.js
@@ -21,99 +21,30 @@ $(document).ready(function(){
     $('#player_mark').text(players[current_player].mark);
   };
 
-  $('.table .col')
-  .bind('click', playMove)
-  .bind('mouseover', hoverCell)
-  .bind('mouseout', leaveCell);
 
-  function hoverCell(ev) {
-    $(this).addClass('hover');
-    return false;
-  };
+  var winningCombo = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [3,5,7],
+    [1,5,9]
+  ];
 
-  function leaveCell(ev) {
-    $(this).removeClass('hover');
-    return false;
-  };
 
-  function playMove(ev) {
-    var cell = $(this);
-    cell
-    .addClass(players[current_player].style)
-    .addClass('marked')
-    .text(players[current_player].mark)
-    .trigger('mouseout')
-    .unbind('click mouseover mouseout');
-
-    if(!checkAndProcessWin()) {
-      current_player = (++current_player) % players.length;
-      initTurn(current_player);
+  var turn = 1;
+  $('.cell').on('click', function(){
+    var self = $(this);
+    if (turn % 2 ==0){
+      self.text('O').addClass('player1');
+    } else {
+      self.text('X').addClass('player2');
     }
-    return false;
-  };
-
-  $.expr[':'].mod = function(el, i, m) {
-    return i % m[3] === 0
-  };
-  $.expr[':'].sub_mod = function(el, i, m) {
-    var params = m[3].split(',');
-    return (i-params[0]) % params[1] === 0
-  };
-
-  // check rows
-  for (var row=1; row <= num_of_rows && !win; ++row ) 
-  {
-    cells_inspected = cells
-    .filter(":lt("+num_of_cols*row+")")
-    .filter(":eq("+(num_of_cols*(row-1))+"),:gt("+(num_of_cols*(row-1))+")")
-    .filter("."+current_class);
-    if ( cells_inspected.length == num_of_cols ) win = true;
-  }
-
-  // check columns
-  for (var col=0; col <= num_of_cols && !win; ++col )
-  {
-    cells_inspected = cells
-    .filter(":sub_mod("+col+","+num_of_rows+")")
-    .filter("."+current_class);
-    if ( cells_inspected.length == num_of_rows ) win = true;
-  }
-  // check diagonals
-  cells_inspected = cells
-  .filter(":mod("+(num_of_rows+1)+")")
-  .filter("."+current_class);
-  if ( cells_inspected.length == num_of_rows ) win = true;
-  else{
-    // From lower right to upper left
-    cells_inspected = cells
-    .filter(":mod("+(num_of_rows-1)+"):not(:last,:first)")
-    .filter("."+current_class);
-    if ( cells_inspected.length == num_of_rows ) win = true;
-  }
-
-//   var winningCombo = [
-//     [1,2,3],
-//     [4,5,6],
-//     [7,8,9],
-//     [1,4,7],
-//     [2,5,8],
-//     [3,6,9],
-//     [3,5,7],
-//     [1,5,9]
-//   ];
-
-
-//   var turn = 1;
-//   $('.col').on('click', function(){
-//     var self = $(this);
-//     if (turn % 2 ==0){
-//       self.text('O').addClass('player1');
-//     } else {
-//       self.text('X').addClass('player2');
-//     }
-//     self.off('click');
-//     turn++;
-//   });
+    self.off('click');
+    turn++;
+  });
 
 });
 
