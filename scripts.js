eee00699ef870e3c8fe34c8bc71a4764adb191e5
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-15 22:29:16 -0400

    index wrapper now responsive. check for draw function created.

--- scripts.js
+++ scripts.js
@@ -11,8 +11,10 @@ $(document).ready(function(){
     var self = $(this);
     if (turn % 2 ==0){
       self.text('o').addClass('player1');
+      document.getElementById('player_name').innerHTML="Player 2";
     } else {
       self.text('x').addClass('player2');
+      document.getElementById('player_name').innerHTML="Player 1";
     }
     self.off('click');
     turn++;
@@ -22,6 +24,10 @@ $(document).ready(function(){
 
 // forEach?
 // 
+
+// var winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
+
+  var maxPlays = 9;
 
   if (checkBoard(0, 1, 2, 'x', state) ||
     checkBoard(3, 4, 5, 'x', state) ||
@@ -34,6 +40,7 @@ $(document).ready(function(){
     )
     { alert('x wins!!');
     $('.cell').off('click');
+
   } else if (checkBoard(0, 1, 2, 'o', state) ||
     checkBoard(3, 4, 5, 'o', state) ||
     checkBoard(6, 7, 8, 'o', state) ||
@@ -43,13 +50,23 @@ $(document).ready(function(){
     checkBoard(1, 4, 7, 'o', state) ||
     checkBoard(2, 5, 8, 'o', state)
     )
-  {
-    console.log('ooo ya!!');
+  { alert('ooo ya!!');
+    $('.cell').off('click');
+
+  } 
+  });
+
+  function checkDraw() {
+    if ($('table').children().text().length == 9) {
+      setTimeout(function() { 
+        alert('Everyone is a winner!!');
+      });
+    };
   }
 
-  });
+
   $('button').bind('click', function() {
-    window.location.reload();    
+    window.location.reload();
   });
 
 });  
