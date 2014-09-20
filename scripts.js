726553f5990c8cb186aa9e30a0e37161c928279b
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-19 23:48:25 -0400

    player now able to win on last turn and draw properly handled

--- scripts.js
+++ scripts.js
@@ -9,54 +9,59 @@ $(document).ready(function() {
   var turn = 1;
   var state;
   var winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
-  var winner;
+  var winnerFound = false;
 
   $('.cell').on('click', function() {
     var self = $(this);
     if (turn % 2 ==0) {
-      self.text('o').addClass('player1');
-      document.getElementById('player_name').innerHTML="Player 2";
-    } else {
       self.text('x').addClass('player2');
-      document.getElementById('player_name').innerHTML="Player 1";
+      $('#player_name').text('Player 2');
+
+    } else {
+      self.text('o').addClass('player1');
+      $('#player_name').text('Player 1');
     }
     self.off('click');
     state = $('.cell').get().map(function(elem) {
       return elem.innerText;
     });
     checkForWin();
-    turn++;    
+    turn++;
   });
 
   function checkForWin() {
     for (var i=0;i<winningCombos.length;i++) {
       if (turn > 1) {
         if (checkBoard(winningCombos[i], 'x', state)) {
-          handleWinner("x");
-        } else if (checkBoard(winningCombos[i], 'o', state)) {
-          handleWinner("x");
-        } else if (turn == 9) {
-          handleDraw();
-          break;
-        }
-      }
-    }
-  }
-  
-  function handleWinner(player) {
-    alert(player + ' wins!!');
-    $('.cell').off('click');    
-  }
+         return handleWinner("x");
+       } else if (checkBoard(winningCombos[i], 'o', state)) {
+         return handleWinner("o");
+       } 
+     } 
+   }
+   if ((turn == 9) && (winnerFound == false)) {
+    return handleDraw();
+   }
+ }
 
-  function handleDraw() {
-    alert("Draw!!");
-    $('.cell').empty();
-    window.location.reload();
-  };
 
-  $('button').bind('click', function() {
-    window.location.reload();
-    
-  });
+ function handleWinner(player) {
+  alert(player + ', you sunk my battle ship!');
+  winnerFound = true;
+  $('.cell').off('click');
+  $('.cell').empty();
+  window.location.reload();   
+}
+
+function handleDraw() {
+  alert('Draw!');
+  $('.cell').empty();
+  window.location.reload();
+};
+
+$('button').bind('click', function() {
+  window.location.reload();
+
+});
 
 });  
